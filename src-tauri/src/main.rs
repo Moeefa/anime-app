b33e37d78a0591ae1780ae98e99b87b60b5fb888
Moeefa <moeefa@protonmail.com>
2024-06-02 21:51:09 -0400

    🐛 fix: transparent window in linux

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,61 +3,66 @@
 
 #[allow(unused_imports)]
 use tauri::{
-    menu::{Menu, MenuItem},
-    window::{Color, Effect, EffectState, EffectsBuilder},
-    Manager, WebviewWindow, WindowEvent,
+  menu::{Menu, MenuItem},
+  window::{Color, Effect, EffectState, EffectsBuilder},
+  Manager, WebviewWindow, WindowEvent,
 };
 
 #[tauri::command]
-fn context_menu(window: WebviewWindow) {
-    let manager = window.app_handle();
-    let menu = Menu::with_items(
-        manager,
-        &[
-            &MenuItem::with_id(manager, "item1", "Menu Item 1", true, None::<&str>).unwrap(),
-            &MenuItem::with_id(manager, "item2", "Menu Item 2", true, None::<&str>).unwrap(),
-        ],
-    )
-    .unwrap();
-
-    window.popup_menu(&menu).unwrap();
-}
-
-#[tauri::command]
 async fn open_settings(app: tauri::AppHandle) {
-    let _webview_window = tauri::WebviewWindowBuilder::new(
-        &app,
-        "settings",
-        tauri::WebviewUrl::App("/#/settings".into()),
-    )
-    .title("Settings")
-    .center()
-    .decorations(false)
-    // .transparent(true)
-    .always_on_top(true)
-    .inner_size(400.0, 500.0)
-    // .effects(
-    //     EffectsBuilder::new()
-    //         .effects([Effect::Mica, Effect::FullScreenUI])
-    //         .build(),
-    // )
-    .build();
+  let _webview_window = tauri::WebviewWindowBuilder::new(
+    &app,
+    "settings",
+    tauri::WebviewUrl::App("/#/settings".into()),
+  )
+  .title("Settings")
+  .center()
+  .decorations(false)
+  .always_on_top(true)
+  .inner_size(400.0, 500.0)
+  .build();
 }
 
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_store::Builder::new().build())
-        .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_os::init())
-        .invoke_handler(tauri::generate_handler![context_menu, open_settings])
-        .on_window_event(|_window, event| match event {
-            WindowEvent::Resized(_e) => {
-                std::thread::sleep(std::time::Duration::from_nanos(1));
-            }
-            _ => {}
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .plugin(tauri_plugin_process::init())
+    .plugin(tauri_plugin_store::Builder::new().build())
+    .plugin(tauri_plugin_shell::init())
+    .plugin(tauri_plugin_http::init())
+    .plugin(tauri_plugin_os::init())
+    .invoke_handler(tauri::generate_handler![open_settings])
+    .on_window_event(|_window, event| match event {
+      WindowEvent::Resized(_e) => {
+        std::thread::sleep(std::time::Duration::from_nanos(1));
+      }
+      _ => {}
+    })
+    .setup(|app| {
+      let main_window = tauri::WebviewWindowBuilder::new(
+        app.app_handle(),
+        "main",
+        tauri::WebviewUrl::App("/".into()),
+      )
+      .title("Rabbit Hole")
+      .resizable(true)
+      .decorations(false)
+      .transparent(true)
+      .inner_size(1000.0, 562.0)
+      .min_inner_size(500.0, 300.0)
+      .center()
+      .effects(
+        EffectsBuilder::new()
+          .effects([Effect::Mica, Effect::FullScreenUI])
+          .build(),
+      );
+
+      #[cfg(target_os = "linux")]
+      let main_window = main_window.transparent(false);
+
+      main_window.build().unwrap();
+
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
